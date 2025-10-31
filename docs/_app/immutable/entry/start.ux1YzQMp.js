7daf415e1cb1b56031b2d561757ff3db20c8dbe2
samuelkotz10 <samuelkotz10@gmail.com>
2025-10-31 18:30:46 +0000

    fix: Corrige caminhos base para o deploy no GitHub Pages

---
+++ docs/_app/immutable/entry/start.ux1YzQMp.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DFqde4qL.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -32,7 +32,7 @@ const config = {
             strict: true
         }),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/me' : '',
+            base: '/me',
         }
 	}
 };
